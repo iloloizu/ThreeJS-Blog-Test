a8d4223fb76f49cdf8b55076b93cd16d5c108ce6
Ilolo Izu <iloloizu97@gmail.com>
2022-04-21 08:21:46 -0500

    working for blog

--- src/App.js
+++ src/App.js
@@ -10,14 +10,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
             <Canvas className='canvas' >
                 <Box/>
                 <ambientLight intensity={1}/>
                 <directionalLight position={[-2,5,2]} intensity={1}/>
                 <OrbitControls enableZoom={false} autoRotate={true}/>
             </Canvas>
-        </p>
+            
       </header>
     </div>
   );
